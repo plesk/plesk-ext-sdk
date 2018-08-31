381bafc09d8f25c876119e42ccd9d02bd420fcf5
Evgenii Karagodin <ekaragodin@plesk.com>
2018-08-31 16:20:38 +0700

    Extract components from template

---
+++ src/App/App.js
@@ -0,0 +1,38 @@
+// Copyright 1999-2018. Plesk International GmbH. All rights reserved.
+
+import { createElement, LocaleProvider, PropTypes } from '@plesk/ui-library';
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { ConfigContext } from '../Config';
+import Route from '../Route';
+
+const App = ({ locale, routes, ...props }) => (
+    <ConfigContext.Provider
+        value={{
+            baseUrl: props.baseUrl,
+        }}
+    >
+        <LocaleProvider messages={locale}>
+            <BrowserRouter basename={props.baseUrl}>
+                <Switch>
+                    {routes.map(({ path, component: Tag, ...routeProps }) => (
+                        <Route
+                            key={path}
+                            path={path}
+                            render={() => <Tag {...props} />}
+                            routes={routes}
+                            {...routeProps}
+                        />
+                    ))}
+                </Switch>
+            </BrowserRouter>
+        </LocaleProvider>
+    </ConfigContext.Provider>
+);
+
+App.propTypes = {
+    routes: PropTypes.array.isRequired,
+    locale: PropTypes.object.isRequired,
+    baseUrl: PropTypes.string.isRequired,
+};
+
+export default App;
