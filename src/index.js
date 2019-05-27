2e1f31f62d0260510a858086573f1d625f46a12c
Evgenii Karagodin <ekaragodin@plesk.com>
2019-05-27 18:26:42 +0700

    Export Router Link as RouterLink

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // Copyright 1999-2018. Plesk International GmbH. All rights reserved.
 
 export * from '@plesk/ui-library';
-export { Link } from 'react-router-dom';
+export { Link as RouterLink } from 'react-router-dom';
 export { ServerForm } from './ServerForm';
 export { ServerList } from './ServerList';
 export { withConfig } from './Config';
